afe152246dbab6956276cb5bd379b3c29c8f0864
Riya <113528211+Riya12goyal@users.noreply.github.com>
2023-07-18 23:25:34 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,54 @@
-const inputbox = document.querySelector('#input-box');
+const daysbtn = document.getElementById('5days');
+const container2 = document.querySelector('.container2');
+const inputbox = document.getElementById('input-box');
+const searchbox = document.querySelector('.search-box');
+const container1 = document.querySelector('.container1');
+ container2.style.display = 'none'; // Hide container2 initially
+
+daysbtn.addEventListener('click', async() => {
+
+    container2.style.display = 'flex';
+	container1.style.display = 'none'
+	searchbox.style.display = 'none'; // Display container2 for 5-day forecast page
+
+  // Call the checkweather5days function here
+ await checkweather5days(inputbox.value, 0, 'c');
+});
+
+async function checkweather5days(city, lon, type) {
+	previousaudio.pause();
+        previousaudio.currentTime = 0;
+  const api_key = '7a21c66e6a225c103f421d1fe84dca41';
+  let url;
+  if (type === 'c') {
+    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}`;
+  }
+  if (type === 'l') {
+    url = `https://api.openweathermap.org/data/2.5/forecast?lat=${city}&lon=${lon}&appid=${api_key}`;
+  }
+  const dayweather = await fetch(`${url}`).then((response) => response.json());
+  let i;
+  for (i = 1; i <= 5; i++) {
+    const humidElement = document.getElementById(`day${i}humid`);
+    humidElement.textContent = dayweather.list[i - 1].main.humidity;
+	const tempElement = document.getElementById(`day${i}temp`);
+    tempElement.textContent = `${Math.round(dayweather.list[i-1].main.temp - 273.15)}`;
+	const windElement = document.getElementById(`day${i}wind`);
+    windElement.textContent = dayweather.list[i - 1].wind.speed;
+	const descElement = document.getElementById(`day${i}desc`);
+    descElement.textContent = dayweather.list[i - 1].weather[0].description;
+     document.getElementById(`day${i}img`).src = `https://openweathermap.org/img/wn/${dayweather.list[i-1].weather[0].icon}.png`;
+  const date =document.getElementById(`date${i}`);
+  
+  const dates = new Date((dayweather.list[(i-1)*8].dt) * 1000);
+  const formattedDate = dates.toLocaleDateString('en-US', {
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric'
+  }); 
+  date.textContent = formattedDate;
+}
+}
 const search_btn = document.getElementById('search-btn');
 const max_temp = document.querySelector('.max_temp');
 const humid = document.getElementById('humid');
@@ -27,7 +77,7 @@ async function checkWeather(city,lon,type){
  if(weatherd.cod === `404`){
 	not_found.style.display = "flex";
 	weather_body.style.display = "none";
-	
+	container2.style.display = 'none';
 	return;
  }
 weather_body.style.display = "flex";
@@ -130,19 +180,34 @@ function getlong(){
 	}
 	return lon;
 }
+
 search_btn.addEventListener('click', ()=>{
 	checkWeather(inputbox.value,0,'c');
+	btn = 's'
 })
 
 loc.addEventListener('click', ()=>{
-	
 	let res_location = confirm('allow the website to access your location?');
-	
+	inputbox.value = 'weather at your place';
+	inputbox.style.fontWeight = 'bold';
 	if(res_location){
 		var lon = getlong();
 		var lat = getlat();
 		checkWeather(lat,lon,'lat');
 	}
+	btn = 'l'
 	
 })
+const backbtn = document.getElementById("backbtn")
 
+backbtn.addEventListener('click', ()=>{
+	container2.style.display = 'none';
+    searchbox.style.display = 'flex'; // Show the search box
+
+    // Additional code to hide other elements if necessary
+    container1.style.display = 'block';
+    weather_body.style.display = 'none';
+    not_found.style.display = 'none';
+	inputbox.value="";
+
+});
